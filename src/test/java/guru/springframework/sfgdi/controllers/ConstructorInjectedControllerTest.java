a5e11d32cbad63895909c059dfa3b1ac6c7e3eed
Lewis <lewisthelifeguard@gmail.com>
2021-09-16 10:26:46 +0100

    Section 3 - Lecture 6 (34) - Dependency Injection Without Spring

---
+++ src/test/java/guru/springframework/sfgdi/controllers/ConstructorInjectedControllerTest.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConstructorInjectedControllerTest {
+    ConstructorInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        //Performing manual dependency injection, mimicking what the Spring framework does automatically.
+        controller = new ConstructorInjectedController(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
---
+++ src/test/java/guru/springframework/sfgdi/controllers/SetterInjectedControllerTest.java
@@ -0,0 +1,21 @@
+package guru.springframework.sfgdi.controllers;
+
+import guru.springframework.sfgdi.services.GreetingServiceImpl;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SetterInjectedControllerTest {
+    SetterInjectedController controller;
+
+    @BeforeEach
+    void setUp() {
+        //Performing manual dependency injection, mimicking what the Spring framework does automatically.
+        controller = new SetterInjectedController();
+        controller.setGreetingService(new GreetingServiceImpl());
+    }
+
+    @Test
+    void getGreeting() {
+        System.out.println(controller.getGreeting());
+    }
+}
