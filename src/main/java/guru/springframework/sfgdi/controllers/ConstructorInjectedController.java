cf6ef591380922fa38b1eeb4d7576241617f61ec
Lewis <lewisthelifeguard@gmail.com>
2021-09-16 13:22:37 +0100

    Section 3 - Lecture 7 (35) - Dependency Injection Using Spring Frame Work

--- src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/ConstructorInjectedController.java
@@ -1,10 +1,14 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class ConstructorInjectedController {
     private final GreetingService greetingService;
 
+    @Autowired  //As of Spring 5 it is no longer required to autowire constructors however it is done here to be comparable with the other 2 controllers to show how they all work.
     public ConstructorInjectedController(GreetingService greetingService) {
         this.greetingService = greetingService;
     }
--- src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
+++ src/main/java/guru/springframework/sfgdi/controllers/PropertyInjectedController.java
@@ -1,8 +1,17 @@
 package guru.springframework.sfgdi.controllers;
 
 import guru.springframework.sfgdi.services.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 
+@Controller
 public class PropertyInjectedController {
+    /*
+    The @Autowired annotation tells the Spring context that this variable (or what ever it is applied to) should be
+    autowired (automatically instantiated) by Spring's dependency injection facilities.  It will require a @Component or
+    a subset of a @Component of that type to be declared so the context knows what can be injected into it.
+     */
+    @Autowired
     public GreetingService greetingService;
 
     public String getGreeting() {
