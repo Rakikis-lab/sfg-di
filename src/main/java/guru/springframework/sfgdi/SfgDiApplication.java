60f7847fd9ab088690509fa0f8f92757206d5a5c
Lewis <lewisthelifeguard@gmail.com>
2021-09-20 16:08:10 +0100

    Section 5 - Lecture 4 (72) - Component Scan

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -4,7 +4,15 @@ import guru.springframework.sfgdi.controllers.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
-
+/*
+The @SpringBootApplication annotation states that this class and the package it is in, are the top level of a Spring
+Boot application, making the package this class is in a "base package".  This has implications for component scanning,
+the scanner will look in base packages and the packages they contain.
+The @ComponentScan annotation can be used to adjust this behaviour by adding in new base packages to scan.  This is done
+as a comma seperated list of Strings inside a {}.  Note that when this is used, it doesn't add new base packages it SETS
+them, so if you do not include the current package it will be removed!!  For example
+@ComponentScan(basePackages = {"guru.springframework.sfgdi", "another.package", "yet.another.package"}
+ */
 @SpringBootApplication
 public class SfgDiApplication {
 
