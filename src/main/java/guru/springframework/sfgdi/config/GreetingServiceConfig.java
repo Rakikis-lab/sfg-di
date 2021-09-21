c011567d272934caea05bbe7f34b16211cd04af4
Lewis <lewisthelifeguard@gmail.com>
2021-09-21 09:06:47 +0100

    Section 5 - Lecture 6 (74) - Primary Beans And Profiles

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -18,6 +18,8 @@ public class GreetingServiceConfig {
     It is possible to supply a name for the bean with a tag (see @Bean source for examples) however by default it will
     take the method name as its own.  Note we don't just have to instantly return the beans we can configure them
     however we want prior to returning.
+    When configuring a bean we can use the @Primary and @Profile annotations on the method returning the bean in
+    exactly the same way as we would on the class if we were using the annotations "normally".
      */
     @Bean
     ConstructorGreetingServiceImpl constructorGreetingServiceImpl() {
