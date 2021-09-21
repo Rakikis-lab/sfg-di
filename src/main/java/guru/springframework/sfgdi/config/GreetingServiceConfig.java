490078d5c29a855146e1f1f613e2758f88096604
Lewis <lewisthelifeguard@gmail.com>
2021-09-21 08:45:46 +0100

    Section 5 - Lecture 5 (73) - Java Configuration Example

---
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -0,0 +1,26 @@
+package guru.springframework.sfgdi.config;
+
+import guru.springframework.sfgdi.services.ConstructorGreetingServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/*
+The @Configuration annotation indicates that this class contains some configuration methods which show how to configure
+beans for use in the Spring context.
+ */
+@Configuration
+public class GreetingServiceConfig {
+    /*
+    The @Bean annotation shows that the method will return a configured bean for use in the Spring context.  This is
+    not typically done with your own code - with annotations being preferred, but more done with third party libraries.
+    Where we want to bring them into the Spring context, as we don't own the code we cannot annotate it, but we can
+    declare parts of it as beans.
+    It is possible to supply a name for the bean with a tag (see @Bean source for examples) however by default it will
+    take the method name as its own.  Note we don't just have to instantly return the beans we can configure them
+    however we want prior to returning.
+     */
+    @Bean
+    ConstructorGreetingServiceImpl constructorGreetingServiceImpl() {
+        return new ConstructorGreetingServiceImpl();
+    }
+}
--- src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingServiceImpl.java
+++ src/main/java/guru/springframework/sfgdi/services/ConstructorGreetingServiceImpl.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgdi.services;
 
 import org.springframework.stereotype.Service;
 
-@Service
+//Removed the @Service annotation to demonstrate Java based configuration with the @Configuration annotation.
 public class ConstructorGreetingServiceImpl implements GreetingService {
     @Override
     public String sayGreeting() {
